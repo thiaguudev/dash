c830d46417ad9403718ce1e18d98a0d02383b0e3
Thiago Luiz Silva - Desenvolvimento <23242@cenl.cal>
2024-01-29 15:40:21 -0300

    initial commit

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -7,7 +7,10 @@ import LineChartExample from "./LineExample";
 export default function Dashboard() {
   return (
     <div className="flex-1">
-      <Flex flexDirection="col" className="flex-1 gap-5 px-14 py-[72px]">
+      <Flex
+        flexDirection="col"
+        className="flex-1 gap-5 px-3 md:px-14 py-[72px]"
+      >
         <UserNav />
         <Example />
         <Flex>
--- src/components/UserNav.tsx
+++ src/components/UserNav.tsx
@@ -1,17 +1,20 @@
 "use client";
 
 import React from "react";
-import { Flex, Icon, Text } from "@tremor/react";
+import { Button, Flex, Icon, Text } from "@tremor/react";
 import { SearchIcon, BellIcon } from "@heroicons/react/outline";
 import Image from "next/image";
 
 export default function UserNav() {
   return (
     <Flex justifyContent="end" className="gap-3 p-5">
-      <Icon icon={SearchIcon} size="lg" className="text-#7C8DB5" />
+      {/* <Icon icon={SearchIcon} size="lg" className="text-#7C8DB5" />
       <Icon icon={BellIcon} size="lg" className="text-#7C8DB5" />
       <Image src="/avatar.png" alt="" width={32} height={32} />
-      <Text className="text-xl">Yara Summers</Text>
+      <Text className="text-xl">Yara Summers</Text> */}
+      <Button size="xl" className="w-[152px] text-xl">
+        Export CSV
+      </Button>
     </Flex>
   );
 }
